9c83c92d020a90584d875badf6d96e40d7c899bf
farmer <onecodefarmer@163.com>
2019-06-25 23:56:35 +0800

    web4

--- Webpack4/demo3/src/index.js
+++ Webpack4/demo3/src/index.js
@@ -1,4 +1,21 @@
 import "./css/index.css"
+import './less/style.less'
+
+import React,{Component} from 'react' // 模块
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <span>React</span>,
+    document.getElementById('root')
+)
+//import './scss/style.scss'
+
+// webpack4.x默认自带支持ES6语法
+import {a,b,c} from "./js/a.js";
+
+console.log(a*b)
+console.log(c) // jsx
+
 import youImg from '../src/images/you.jpg' // 引入图片
 let oImg = new Image() // IMG对象
 oImg.onload = function(){
---
+++ Webpack4/demo3/src/js/a.js
@@ -0,0 +1,4 @@
+export const a = 12;
+export const b = 5;
+
+export const c = <h3>JSX语法</h3>
